67ea7993afcb204e9c93ad5a5cd2dad11d689be6
zhupengfei <zhupengfei2334@163.com>
2020-04-01 20:07:28 +0800

    #init: resolve

---
+++ src/main/java/com/zpp/compile/ClassDecodeResolve.java
@@ -0,0 +1,8 @@
+package com.zpp.compile;
+
+/**
+ * @author steven.zhu 2020/4/1 20:03.
+ * @类描述：
+ */
+public class ClassDecodeResolve {
+}
---
+++ src/main/java/com/zpp/compile/ClassDecodeResolves.java
@@ -0,0 +1,14 @@
+package com.zpp.compile;
+
+/**
+ * @author steven.zhu 2020/4/1 20:02.
+ * @类描述：
+ */
+public class ClassDecodeResolves {
+
+
+
+    public ClassDecodeResolve getResolve() {
+        return null;
+    }
+}
